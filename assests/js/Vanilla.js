bc376f89ad7b076180291908fb990216bf6cb650
George Alvarado <georgealvarado305@gmail.com>
2023-05-30 06:46:50 -0400

    updated about page

--- assests/js/Vanilla.js
+++ assests/js/Vanilla.js
@@ -20,4 +20,6 @@ animatedRight.forEach((el) => observer.observe(el));
 const animatedIntro = document.querySelectorAll(".title-intro");
 animatedIntro.forEach((el) => observer.observe(el));
 
+const animatedIntro2 = document.querySelectorAll(".title-intro2");
+animatedIntro2.forEach((el) => observer.observe(el));
 // nav scroll
