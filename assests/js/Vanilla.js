e35b7f884855e8e3e05325eddb28eeef185be253
George Alvarado <georgealvarado305@gmail.com>
2023-06-06 19:27:16 -0400

    updated slideshow

--- assests/js/Vanilla.js
+++ assests/js/Vanilla.js
@@ -22,4 +22,37 @@ animatedUp.forEach((el) => observer.observe(el));
 
 const animatedUp2 = document.querySelectorAll(".fade-up2");
 animatedUp2.forEach((el) => observer.observe(el));
-// nav scroll
+
+// contact form
+
+function sendEmail() {
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "username",
+    Password: "password",
+    To: "them@website.com",
+    From: "you@isp.com",
+    Subject: "This is the subject",
+    Body: "And this is the body",
+  }).then((message) => alert(message));
+}
+
+// slideshow
+
+const swiper = new Swiper(".swiper", {
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
