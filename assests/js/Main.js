b7809d80b51a9254f248cb1f8abca848c8a66d8a
George Alvarado <georgealvarado305@gmail.com>
2023-06-07 07:01:13 -0400

    added pop up gallery on about page

--- assests/js/Main.js
+++ assests/js/Main.js
@@ -27,3 +27,15 @@ $(".fa-instagram").hover(
     $(this).removeClass("fa-bounce");
   }
 );
+
+// image pop up for gallery
+
+$(document).ready(function () {
+  var popup_btn = $(".popup-btn");
+  popup_btn.magnificPopup({
+    type: "image",
+    gallery: {
+      enabled: true,
+    },
+  });
+});
